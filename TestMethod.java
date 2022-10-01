ecdd06f06b0986e138d571db36afa99b97c9c7d3
yueguanga <106826967+yueguanga@users.noreply.github.com>
2022-10-01 14:24:59 +0800

    Add files via upload

---
+++ TestMethod.java
@@ -0,0 +1,13 @@
+//最简单的方法
+
+public class TestMethod {
+    public static void main(String[] args) {
+        saylove();
+    }
+
+    public static void saylove() {
+        System.out.println("I");
+        System.out.println("LOVE");
+        System.out.println("YOU");
+    }
+}
